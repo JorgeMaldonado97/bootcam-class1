9ff1c6b7ba0da06a522af5405e0952c95e457f1d
Jorge Duran M <Jorge Duran M@LAPTOP-3L3DSMOC>
2022-07-07 09:50:53 -0500

    Clase 1
    inicio protalento

---
+++ pruebas/src/main/java/pruebas/TipoDeDatos.java
@@ -0,0 +1,26 @@
+package pruebas;
+
+public class TipoDeDatos {
+	
+//argumentos o parametros q vienen desde afuera
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Tipos de datos
+		//edad > numericos
+		// byte < short < int < long < float < double
+		
+		byte edad = 10;
+		short edadshort = 10;
+		int edadint = 10;
+		long edadlong= 10;
+		float edadfloat = 10;
+		double edaddouble = edadfloat;
+		
+	
+		
+		
+	}
+
+}
